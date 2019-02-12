72b2aa37f1bffc4709835acbbb3154e1f5ada939
BrannanC <brannanconrad@gmail.com>
2019-02-12 15:43:47 -0600

    Del and Put

--- Posts/posts-router.js
+++ Posts/posts-router.js
@@ -41,7 +41,45 @@ router.post('/', (req, res) => {
                 posts.findById(idObj.id)
                 .then(post => res.status(201).json({ post }))
             })
-            .catch(err => res.status(500).json({ error: "There was an error while saving the post to the database" }))
+            .catch(err => {
+                console.log(err);
+                res.status(500).json({ error: "There was an error while saving the post to the database" })
+            })
+    }
+});
+
+router.delete('/:id', (req, res) => {
+    posts
+        .remove(req.params.id)
+        .then(isDel => {
+            isDel ?
+            res.status(204).end()
+            : res.status(404).json({ message: "The post with the specified ID does not exist." });
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json({ error: "The post could not be removed" })
+        })
+});
+
+router.put('/:id', (req, res) => {
+    const changes = req.body;
+    if(!changes.title || !changes.contents){
+        res.status(400).json({ errorMessage: "Please provide title and contents for the post." })
+    } else {
+        posts
+            .update(req.params.id, changes)
+            .then(didUpdate => {
+                didUpdate > 0 ?
+                posts
+                    .findById(req.params.id)
+                    .then(post => res.status(200).json({ post }))
+                : res.status(404).json({ message: "The post with the specified ID does not exist." })
+            })
+            .catch(err => {
+                console.log(err);
+                res.status(500).json({ error: "The post information could not be modified." })
+            })
     }
 });
 
